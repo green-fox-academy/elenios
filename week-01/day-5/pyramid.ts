811c618640d74d2ed9d6620b7592976ee8878774
Elenios <gabor.dobrozemszky@hotmail.com>
2018-08-05 00:09:30 +0200

    ultra optimalization

--- week-01/day-5/pyramid.ts
+++ week-01/day-5/pyramid.ts
@@ -1,22 +1,6 @@
 'use strict';
 /*
-let lineCount: number = 4;
-let csillagocska: string = "*"
-let helyecske: string = " "
 
-while (lineCount - 1 > 0) {
-    lineCount--;
-
-    while (lineCount - 1 > 0){
-        helyecske += " ";
-    }
-    while (1-lineCount  > 0){
-            lineCount++;
-            csillagocska += "**";
-            
-    }   
-}   console.log(csillagocska + helyecske);
-*/
 
 let lineCount: number = 4;
 let csillagocska: string = "*"
@@ -37,4 +21,19 @@ while (lineCount > 0) {
         i++
     } 
     console.log(helyecske + csillagocska); 
+}
+*/
+let lineCount: number = 4;
+let csillagocska: string = "*"
+let helyecske: string = ""
+
+while (lineCount > 0) {
+    helyecske = ""
+    
+    for (let i: number = 0; lineCount - i - 1 > 0; i++){
+        helyecske += " ";
+    } 
+    lineCount--;
+    console.log(helyecske + csillagocska); 
+    csillagocska += "**";
 }
