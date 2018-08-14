a89b234d0558489927f6404e20fe9b8d7c2830a7
Elenios <gabor.dobrozemszky@hotmail.com>
2018-08-14 17:09:08 +0200

    uniqueIPs feature added

--- week-03/day-2/logs.ts
+++ week-03/day-2/logs.ts
@@ -11,17 +11,36 @@ const charBase = 'utf-8';
 
 function uniqueIPs(file: string): any {
 
-  let baseArray = fs.readFileSync(file, charBase).split('\r\n');
-      let IPs = baseArray.forEach(element => {
-
+  let baseArray = (fs.readFileSync(file, charBase).split('\r\n'));
+  let baseArray2 = [];
+  let IPs = [];
+  let uniqueIPs = [];
+  baseArray.forEach(element => {
+    baseArray2.push(element.split('   '));
   });
-/*
-  try {
-    return true;
-  } catch {
-    return false;
+  baseArray2.forEach(element => {
+    IPs.push(element[1]);
+  });
+
+  for (let i = 0; i < IPs.length; i++) {
+    if (uniqueIPs.indexOf(IPs[i]) <= -1) {
+      uniqueIPs.push(IPs[i]);
+    }
   }
-  */
+  return uniqueIPs;
+
+  /*uniqueIPs = IPs.filter(function (elem, index, self) {
+    return index === self.indexOf(elem);
+  })*/
+
+
+  /*
+    try {
+      return true;
+    } catch {
+      return false;
+    }
+    */
 }
 
 console.log(uniqueIPs('log.txt'));
