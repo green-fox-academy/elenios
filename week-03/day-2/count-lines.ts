796171139038036445a7eb5eb014527ae2ada018
Elenios <gabor.dobrozemszky@hotmail.com>
2018-08-14 13:51:18 +0200

    added count-lines with try catch

---
+++ week-03/day-2/count-lines.ts
@@ -0,0 +1,26 @@
+declare function require(path: string): any;
+export { };
+'use strict';
+
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+
+function countLines(fileName: string): any {
+  const fs = require('fs');
+  let counter = 0;
+
+  try {
+    let fileContent = fs.readFileSync(fileName, 'utf-8');
+    fileContent.split('\r\n').forEach(function (element) {
+      counter++
+    });
+    return counter;
+
+  } catch (e) {
+  return 0;
+}
+}
+
+console.log(countLines('myspace.txt'));
