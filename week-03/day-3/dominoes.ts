6bb30e27ff184b58340f4f7bf44caee130071db7
Elenios <gabor.dobrozemszky@hotmail.com>
2018-08-15 16:59:21 +0200

    diceset DONE, dominoes turned out to be not done. cry

--- week-03/day-3/dominoes.ts
+++ week-03/day-3/dominoes.ts
@@ -5,8 +5,8 @@ function initializeDominoes(): Domino[] {
   dominoes.push(new Domino(5, 2));
   dominoes.push(new Domino(4, 6));
   dominoes.push(new Domino(1, 5));
-  dominoes.push(new Domino(6, 7));
   dominoes.push(new Domino(2, 4));
+  dominoes.push(new Domino(6, 7));
   dominoes.push(new Domino(7, 1));
   return dominoes;
 }
