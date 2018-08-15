39e2799dd829119c903697bfe24277a23be0a0ca
Elenios <gabor.dobrozemszky@hotmail.com>
2018-08-15 22:57:15 +0200

    animal class created, not sure about fix starter values, fingers are crossed

---
+++ week-03/day-3/animal.ts
@@ -0,0 +1,28 @@
+/*
+Create an Animal class
+Every animal has a hunger value, which is a whole number
+Every animal has a thirst value, which is a whole number
+when creating a new animal object these values are created with the default 50 value
+Every animal can eat() which decreases their hunger by one
+Every animal can drink() which decreases their thirst by one
+Every animal can play() which increases both by one
+*/
+
+class Animal {
+  hunger: number;
+  thirst: number;
+  constructor(hunger: number, thirst: number) {
+    this.hunger = 50;
+    this.thirst = 50;
+  }
+  eat() {
+    this.hunger--
+  }
+  drink() {
+    this.thirst--
+  }
+  play() {
+    this.hunger++
+    this.thirst++
+  }
+}
