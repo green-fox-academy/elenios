a2feece3fe36ae755f978f38837e44c6ba84c349
Elenios <gabor.dobrozemszky@hotmail.com>
2018-09-20 17:35:29 +0200

    feat()post and vote

--- week-07/day-04-05/Reddit/application.js
+++ week-07/day-04-05/Reddit/application.js
@@ -48,12 +48,57 @@ app.post('/posts/', jsonParser, (req, res) => {
   let owner = req.body.owner;
 
   conn.query(`INSERT INTO post (title, url, owner) values ('${title}', '${url}', '${owner}');`, (err, result) => {
+
+    let newId = result.insertId;
     if (err) {
       console.log('failure', err.message);
       return;
     }
-    res.json({
-      result: result
+    console.log('Succesfully added');
+
+    conn.query(`SELECT * FROM post WHERE post_id = ${newId};`, (err, result2) => {
+
+      res.json({
+        result: result2
+      });
     });
   });
+});
+
+app.put('/posts/:id/upvote', jsonParser, (req, res) => {
+  let id = req.params.id;
+
+  conn.query(`UPDATE post SET vote = vote + 1  WHERE post_id = ${id};`, (err, result) => {
+    if (err) {
+      console.log('failure', err.message);
+      return;
+    }
+    conn.query(`SELECT * FROM post WHERE post_id = ${id};`, (err, result2) => {
+
+      res.json({
+        result: result2
+      });
+    });
+  });
+});
+
+app.put('/posts/:id/downvote', jsonParser, (req, res) => {
+  let id = req.params.id;
+
+  conn.query(`UPDATE post SET vote = vote - 1  WHERE post_id = ${id};`, (err, result) => {
+    if (err) {
+      console.log('failure', err.message);
+      return;
+    }
+    conn.query(`SELECT * FROM post WHERE post_id = ${id};`, (err, result2) => {
+
+      res.json({
+        result: result2
+      });
+    });
+  });
+});
+
+app.delete('/posts/:id', (req, res) => {
+  
 });
