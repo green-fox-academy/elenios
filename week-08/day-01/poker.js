80f91fa977cf1a936535a1a0de55ea35c0771460
Elenios <gabor.dobrozemszky@hotmail.com>
2018-09-24 16:39:53 +0200

    refactor

--- week-08/day-01/poker.js
+++ week-08/day-01/poker.js
@@ -1,23 +1,38 @@
 'use strict';
 
-function addTwoNumbers(firstNumber, secondNumber) {
+const values = [ '2', '3', '4', '5', '6', '7', '8', '9', 't', 'j', 'd', 'k', 'a' ];
+const colours = [ 'h', 's', 'd', 'c' ];
+
+function addTwoNumbers( firstNumber, secondNumber ) {
   return firstNumber + secondNumber;
 }
 
-function isValid(card) {
-  const values = ['2', '3', '4', '5', '6', '7', '8', '9', 't', 'j', 'd', 'k', 'a'];
-  const colours = ['h', 's', 'd', 'c'];
-
-  if (values.indexOf(card.charAt(0).toLowerCase()) !== -1 && colours.indexOf(card.charAt(1).toLowerCase()) !== -1) {
+function isValid( card ) {
+  if ( values.indexOf( card.charAt( 0 ).toLowerCase() ) !== -1 && colours.indexOf( card.charAt( 1 ).toLowerCase() ) !== -1 ) {
     return true;
   }
 }
 
-function isPair(card1, card2) {
-  
+function isPair( card1, card2 ) {
+  if ( isValid( card1 ) && isValid( card2 ) ) {
+    if ( card1.charAt( 0 ) === card2.charAt( 0 ) ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
+
+function isDrill () {
+
+}
+
+function isFullHouse 
+
 
 module.exports = {
   addTwoNumbers,
-  isValid
+  isValid,
+  isPair
 }
+
--- week-08/day-01/test.js
+++ week-08/day-01/test.js
@@ -1,29 +1,41 @@
 'use strict';
 
-const tape = require('tape');
-const businessLogic = require('./poker')
+const tape = require( 'tape' );
+const businessLogic = require( './poker' )
 
-tape.test('One plus one equals two', test => {
-  test.equals(2, businessLogic.addTwoNumbers(1, 1));
+tape.test( 'One plus one equals two', test => {
+  test.equals( 2, businessLogic.addTwoNumbers( 1, 1 ) );
   test.end();
-});
+} );
 
-tape.test('is it a valid card?', test => {
-  test.ok(businessLogic.isValid('3C'), 'It\'s a valid card');
+tape.test( 'is it a valid card?', test => {
+  test.ok( businessLogic.isValid( '3C' ), 'It\'s a valid card' );
   test.end();
-});
+} );
 
-tape.test('is it a valid card?', test => {
-  test.notOk(businessLogic.isValid('0S'), 'It\'s not valid card');
+tape.test( 'is it a valid card?', test => {
+  test.notOk( businessLogic.isValid( '0S' ), 'It\'s not valid card' );
   test.end();
-});
+} );
 
-tape.test('is it a valid card?', test => {
-  test.notOk(businessLogic.isValid('TT'), 'It\'s not valid card');
+tape.test( 'is it a valid card?', test => {
+  test.notOk( businessLogic.isValid( 'TT' ), 'It\'s not valid card' );
   test.end();
-});
+} );
 
-tape.test('is it a valid card?', test => {
-  test.notOk(businessLogic.isValid('3T'), 'It\'s not valid card');
+tape.test( 'is it a valid card?', test => {
+  test.notOk( businessLogic.isValid( '3T' ), 'It\'s not valid card' );
   test.end();
-});
+} );
+
+tape.test( 'Are the cards pair?', test => {
+  test.ok( businessLogic.isPair( '3C', '3S' ), 'They are a pairs' );
+  test.end();
+} );
+
+tape.test( 'Are the cards pair?', test => {
+  test.notOk( businessLogic.isPair( 'JS', 'TC' ), 'They are not a pairs' );
+  test.end();
+} );
+
+
