b002a9dfbd3454c940e21ae2a5684ffd999da0bf
Elenios <gabor.dobrozemszky@hotmail.com>
2018-09-26 17:02:16 +0200

    lollypop buying and auto candy creating added

--- week-08/day-02/game.js
+++ week-08/day-02/game.js
@@ -1,8 +1,25 @@
 'use-strict';
 
-const candies = document.querySelector('.candies');
-const candyCreater = document.querySelector('.create-candies');
+const candies = document.querySelector( '.candies' );
+const candyCreater = document.querySelector( '.create-candies' );
+const lollypops = document.querySelector( '.lollypops' );
+const lollypopBuyer = document.querySelector( '.buy-lollypops' );
+const lollypop = '🍭';
 
-candyCreater.addEventListener('click', () => {
-candies.innerHTML++;
-});
+candyCreater.addEventListener( 'click', () => {
+  candies.innerHTML++;
+} );
+
+lollypopBuyer.addEventListener( 'click', () => {
+  if ( candies.innerHTML > 9 ) {
+    lollypops.innerHTML += lollypop;
+    candies.innerHTML -= 10;
+  }
+} );
+
+setInterval( () => {
+  if ( lollypops.innerHTML.lastIndexOf( '🍭' ) >= 18 ) {
+    candies.innerHTML++;
+  } 
+}, 1000 );
+
