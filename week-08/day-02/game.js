38bc1cd2b9c8998995e43d974d613503c50ffe1f
Elenios <gabor.dobrozemszky@hotmail.com>
2018-09-26 22:43:33 +0200

    lollypop finished

--- week-08/day-02/game.js
+++ week-08/day-02/game.js
@@ -5,21 +5,41 @@ const candyCreater = document.querySelector( '.create-candies' );
 const lollypops = document.querySelector( '.lollypops' );
 const lollypopBuyer = document.querySelector( '.buy-lollypops' );
 const lollypop = '🍭';
+const speed = document.querySelector( '.speed' );
+let time = 0;
+const candyRain = document.querySelector( '.candy-machine' );
+let candy = 0;
+let candyGeneration = 0;
+let multiplicator = 1;
 
 candyCreater.addEventListener( 'click', () => {
-  candies.innerHTML++;
+  candy++;
+  candies.innerHTML = candy;
 } );
 
 lollypopBuyer.addEventListener( 'click', () => {
+  candyGeneration = Math.floor(lollypops.innerHTML.length / 18);
   if ( candies.innerHTML > 9 ) {
     lollypops.innerHTML += lollypop;
-    candies.innerHTML -= 10;
+    candy -= 10;
+    candies.innerHTML = candy;
   }
 } );
 
 setInterval( () => {
-  if ( lollypops.innerHTML.lastIndexOf( '🍭' ) >= 18 ) {
-    candies.innerHTML++;
-  } 
+  candy += candyGeneration * multiplicator;
+  candies.innerHTML = candy;
+  time++;
+  speed.innerHTML = candies.innerHTML / time;
+  if (candy >= 10000) {
+    alert('Congratulations! You\'ve reached 10.000 candies!')
+  }
 }, 1000 );
 
+candyRain.addEventListener( 'click', () => {
+  if (multiplicator === 1) {
+    multiplicator = 10;
+  } else {
+    multiplicator += 10;
+  }
+} );
