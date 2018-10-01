2a5f813a84570422ff938cce3fdc8f5a3421b67e
Elenios <gabor.dobrozemszky@hotmail.com>
2018-10-01 15:19:23 +0200

    feat(modify meal) added

--- week-09/day-01/calorie/routes.js
+++ week-09/day-01/calorie/routes.js
@@ -59,4 +59,46 @@ app.post( '/drax/meal/', jsonParser, ( req, res ) => {
   }
 } );
 
+app.delete( '/drax/meal/', jsonParser, ( req, res ) => {
+  let id = req.body.meal_id;
+
+  if ( req.body.meal_id ) {
+
+    conn.query( `DELETE FROM calorietable WHERE meal_id = ${ id };`, ( err, result ) => {
+
+      if ( err ) {
+        console.log( 'failure', err.message );
+        return;
+      }
+      console.log( `Succesfully removed meal with id ${ id }` );
+
+    } );
+  } else {
+    res.status( 400 ).json( { error: 'No input was given' } ).send();
+  }
+} );
+
+app.put( '/drax/:id/', jsonParser, ( req, res ) => {
+  let id = req.params.id;
+  let amount = req.query.amount;
+
+  if ( req.params.id && req.query.amount ) {
+
+    conn.query( `UPDATE calorietable SET amount = '${amount}' WHERE meal_id = ${ id };`, ( err, result ) => {
+      if ( err ) {
+        console.log( 'failure', err.message );
+        return;
+      }
+      conn.query( `SELECT * FROM calorietable WHERE meal_id = ${ id };`, ( err, result2 ) => {
+
+        res.json( {
+          result: result2
+        } );
+      } );
+    } );
+  } else {
+    res.status( 400 ).json( { error: 'No input was given' } ).send();
+  }
+} );
+
 module.exports = app;
