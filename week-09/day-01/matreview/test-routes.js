f49f5c4d0b860e24e670d1fc755497532d5bfc8f
Elenios <gabor.dobrozemszky@hotmail.com>
2018-10-01 12:46:29 +0200

    fix(tests)

--- week-09/day-01/matreview/test-routes.js
+++ week-09/day-01/matreview/test-routes.js
@@ -8,30 +8,28 @@ const routeModule = require( './routes' );
 test( 'groot endpoint', ( t ) => {
   request( routeModule )
     .get( '/groot/' )
-    .set( 'Accept', 'application/json/' )
-        .expect( 400 )
-    .expect( {} )
+    .expect( 'Content-Type', /json/ )
+    .expect( 400 )
     .end( ( err, res ) => {
       if ( err ) {
         t.error( err );
       }
-      t.same( res.body, { error: 'I am Groot!' } );
+      t.deepEquals( res.body, { error: "I am Groot!" } );
       t.end();
     } );
 } );
 
-  // test( 'one and one unit test', ( t ) => {
-  //   let message = 'Hello there general Kenobi'
-  //   request( routeModule )
-  //     .get( '/groot/?message=' + message )
-  //     .set( 'Accept', 'application/json/' )
-  //     .expect( 200 )
-  //     .expect( {} )
-  //     .end( ( err, res ) => {
-  //       if ( err ) {
-  //         t.error( err );
-  //       }
-  //       t.same( res.body, { received: message, translated: 'I am Groot!' } );
-  //       t.end();
-  //     } ); 
-  // } );
+test( 'one and one unit test', ( t ) => {
+  let message = 'Hello there general Kenobi'
+  request( routeModule )
+    .get( '/groot/?message=' + message )
+    .expect( 'Content-Type', /json/ )
+    .expect( 200 )
+    .end( ( err, res ) => {
+      if ( err ) {
+        t.error( err );
+      }
+      t.same( res.body, { received: message, translated: 'I am Groot!' } );
+      t.end();
+    } );
+} );
